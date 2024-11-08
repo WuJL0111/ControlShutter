3f280c9ef77b17d0b1a333e8dbb0711d71440c17
wujialiang <wujialiang0111@163.com>
2024-11-08 17:05:54 +0800

    基于硬件聚英网络继电器，添加WPF形式的门控程序

---
+++ ControlDoor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ControlDoor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ControlDoor/View/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlDoor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void StartFadeIn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    // 启动渐入动画
+        //    Storyboard fadeInStoryboard = (Storyboard)this.Resources["FadeInStoryboard"];
+        //    fadeInStoryboard.Begin();
+        //}
+
+        //private void StartFadeOut_Click(object sender, RoutedEventArgs e)
+        //{
+        //    // 启动渐出动画
+        //    Storyboard fadeOutStoryboard = (Storyboard)this.Resources["FadeOutStoryboard"];
+        //    fadeOutStoryboard.Begin();
+        //}
+    }
+}
