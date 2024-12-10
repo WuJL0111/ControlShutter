fa8ce9530d498cb6990182996ba48b842604e944
wujialiang <wujialiang0111@163.com>
2024-12-10 17:40:06 +0800

    蓬莱门控

---
+++ ControlDoors/Common/ClickRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlDoors.Common
+{
+    public class ClickRequest
+    {
+        public int status { get; set; }
+
+        public string msg { get; set; }
+    }
+}
--- ControlDoors/View/DeviceStatePage.xaml.cs
+++ ControlDoors/View/DeviceStatePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControlDoors.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,57 @@ namespace ControlDoors.View
         public DeviceStatePage()
         {
             InitializeComponent();
+        }
+
+        private void ConnectPullDoor_Click(object sender, RoutedEventArgs e)
+        {
+            ClickRequest request = new ClickRequest();
+            request.status = 211;
+            if (request.status == 200)
+            {
+                SolidColorBrush brush1 = new SolidColorBrush();
+                SolidColorBrush brush2 = new SolidColorBrush();
+                brush1.Color = Colors.LightGreen;
+                PullDoorOnlineCircle.Fill = brush1;
+                brush2.Color = Colors.Gray;
+                PullDoorOfflineCircle.Fill = brush2;
+                ConnectPullDoor.Content = "断 开";
+            }
+            else
+            {
+                SolidColorBrush brush1 = new SolidColorBrush();
+                SolidColorBrush brush2 = new SolidColorBrush();
+                brush1.Color = Colors.Gray;
+                PullDoorOnlineCircle.Fill = brush1;
+                brush2.Color = Colors.Red;
+                PullDoorOfflineCircle.Fill = brush2;
+                ConnectPullDoor.Content = "连 接";
+            }
+        }
+
+        private void ConnectShutterDoor_Click(object sender, RoutedEventArgs e)
+        {
+            ClickRequest request = new ClickRequest();
+            if (request.status == 200)
+            {
+                SolidColorBrush brush1 = new SolidColorBrush();
+                SolidColorBrush brush2 = new SolidColorBrush();
+                brush1.Color = Colors.LightGreen;
+                ShutterDoorOnlineCircle.Fill = brush1;
+                brush2.Color = Colors.Gray;
+                ShutterDoorOfflineCircle.Fill = brush2;
+                ConnectShutterDoor.Content = "断 开";
+            }
+            else
+            {
+                SolidColorBrush brush1 = new SolidColorBrush();
+                SolidColorBrush brush2 = new SolidColorBrush();
+                brush1.Color = Colors.Gray;
+                ShutterDoorOnlineCircle.Fill = brush1;
+                brush2.Color = Colors.Red;
+                ShutterDoorOfflineCircle.Fill = brush2;
+                ConnectShutterDoor.Content = "连 接";
+            }
         }
     }
 }
